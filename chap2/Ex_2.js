0223381c56f2bb5ddadd30590c1c8d73dc749570
ahinsyar <ahinsyar@gmail.com>
2020-12-23 18:29:23 +0900

    Day2

--- chap2/Ex_2.js
+++ chap2/Ex_2.js
@@ -12,9 +12,33 @@ function inconsistentRead(filename, callback){
     }
 }
 
+//
+function inconsistentReadSync(filename, callback){
+    if(cache[filename]){
+        callback(cache[filename])
+    }else{
+        fs.readFileSync(filename, 'utf8', (err, data)=>{
+            cache[filename] = data
+            callback(data)
+        })
+    }
+}
+
+//완전 비동기화
+function inconsistentReadAsync(filename, callback){
+    if(cache[filename]){
+        process.nextTick(() => callback(cache[filename]))
+    }else{
+        fs.readFile(filename, 'utf8', (err, data)=>{
+            cache[filename] = data
+            callback(data)
+        })
+    }
+}
+
 function createFileReader(filename){
     const listeners = [];
-    inconsistentRead(filename, value =>{
+    inconsistentReadAsync(filename, value =>{
         listeners.forEach(listener => listener(value));
     })
 
@@ -23,6 +47,11 @@ function createFileReader(filename){
     }
 }
 
+//비동기 방식으로 동작했을때 2번째를 읽어 오지 못함
+//2번째를 읽기 전에 프로그램이 끝나버려서 실행 시간에 맞추지 못하기 때문
+//하여 위에 sync를 붙인 동기 방식으로 변경 함
+//하지만 동기 방식으로 변경하면 파일을 읽은 후에 다음 명령을 실행 하므로
+//프로그램의 성능이 느려짐
 const reader1 = createFileReader('data.txt')
 reader1.onDataReady(data =>{
     console.log('First call data: '+data)
@@ -31,6 +60,6 @@ reader1.onDataReady(data =>{
 
     const reader2 = createFileReader('data.txt');
     reader2.onDataReady(data =>{
-        console.log('Second Call data:'+data);
+        console.log('Second Call data: '+data);
     })
 })
